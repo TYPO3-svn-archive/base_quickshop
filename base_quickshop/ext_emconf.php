c4b1615ca5ea12e796235dd5b88fd5e0b40fd447
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-25 23:49:34 +0000

    clean up
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_quickshop/trunk@80048 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ base_quickshop/ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "base_quickshop".
+#
+# Auto generated 30-06-2010 10:55
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Quick-Shop - Template',
+	'description' => 'Basic Template for TYPO3 - espacially for the extension Quick Shop (quick_shop)',
+	'category' => 'templates',
+	'author' => 'Dirk Wildt - Die Netzmacher',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'shy' => '',
+	'dependencies' => 'automaketemplate',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '3.0.3',
+	'constraints' => array(
+		'depends' => array(
+			'automaketemplate' => '',
+			'typo3' => '4.5.0-4.7.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+			't3jquery' => '',
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4963";s:11:"INSTALL.txt";s:4:"eea3";s:12:"ext_icon.gif";s:4:"2501";s:14:"ext_tables.php";s:4:"cefc";s:14:"doc/manual.pdf";s:4:"bcdc";s:14:"doc/manual.sxw";s:4:"da09";s:19:"res/html/index.html";s:4:"5a95";s:22:"res/html/css/basic.css";s:4:"d343";s:43:"res/html/css/background/wrap-horizontal.gif";s:4:"2f47";s:22:"res/images/favicon.ico";s:4:"8e17";s:56:"res/images/01_rootpage/quick_shop_header_image_210px.jpg";s:4:"6cd7";s:34:"res/pages/01_rootpage/tsConfig.txt";s:4:"f566";s:56:"res/pages/01_rootpage/02_quickshop_products/tsConfig.txt";s:4:"187f";s:47:"res/pages/01_rootpage/03_libraries/tsConfig.txt";s:4:"0d35";s:57:"res/pages/01_rootpage/03_libraries/04_header/tsConfig.txt";s:4:"187f";s:57:"res/pages/01_rootpage/03_libraries/05_footer/tsConfig.txt";s:4:"187f";s:46:"res/pages/01_rootpage/typoscript/constants.txt";s:4:"05e7";s:42:"res/pages/01_rootpage/typoscript/setup.txt";s:4:"09b3";s:35:"static/base_quickshop/constants.txt";s:4:"3a00";s:31:"static/base_quickshop/setup.txt";s:4:"019c";}',
+);
+
+?>
